91363cc414bd7898c866efc623d05f1c75de09c8
subinpsn <62749111+subinpsn@users.noreply.github.com>
2021-06-30 20:45:44 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email, Length
+from wtforms.validators import DataRequired, URL, Length
 from flask_ckeditor import CKEditorField
 
 ##WTForm
@@ -12,7 +12,7 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8, message="Minimum 8 Characters")])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
